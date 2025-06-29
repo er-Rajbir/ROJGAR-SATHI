8624589934d81effc5a49cb6ddea8d3cc4d28f23
yuvraj sohal <sohalyuvraj05@gmail.com>
2025-06-29 19:22:50 +0530

    Branch created

--- ROJGAR_SATHI/hunarbaaz/views.py
+++ ROJGAR_SATHI/hunarbaaz/views.py
@@ -4,7 +4,14 @@ from django.shortcuts import render
 # Home page view
 def home(request):
     categories = ['Electrician', 'Plumber', 'Technician', 'Construction', 'Painter', 'Welder']
-    return render(request, 'home.html', {'categories': categories})
+    reviews = [
+    {"text": "Amazing service! Got a skilled plumber within minutes.", "name": "Client A", "stars": 4},
+    {"text": "Quick and reliable electrician. Fully satisfied!", "name": "Client B", "stars": 5},
+    {"text": "Professional and polite workers!", "name": "Client C", "stars": 4},
+    {"text": "Very helpful during emergency work.", "name": "Client D", "stars": 5},
+]
+
+    return render(request, 'hunarbaaz/home.html', {'categories': categories, 'reviews':reviews})
 
 # Karigar list page
 def karigar_list(request):
