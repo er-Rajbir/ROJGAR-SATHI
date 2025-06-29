1e3d20768d85a19b1775a4c494f8efa7fa15991a
er-Rajbir <anandrajbir13@gmail.com>
2025-06-29 12:45:16 +0530

    templates done with base.html of navbar only

--- ROJGAR_SATHI/hunarbaaz/urls.py
+++ ROJGAR_SATHI/hunarbaaz/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     
-    path('',views.home,name='home')
+    path('', views.home, name='home'),
+    path('karigars/', views.karigar_list, name='karigar_list'),
+    path('employer/register/', views.employer_register, name='employer_register'),
+    path('login/', views.login_view, name='login'),
 ]
--- ROJGAR_SATHI/hunarbaaz/views.py
+++ ROJGAR_SATHI/hunarbaaz/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render,HttpResponse
+from django.shortcuts import render
+
+# Home page view
 def home(request):
-    return HttpResponse('hello rojgar sathi')
+    return render(request, 'hunarbaaz/home.html')
+
+# Karigar list page
+def karigar_list(request):
+    return render(request, 'hunarbaaz/karigar_list.html')
+
+# Employer registration page
+def employer_register(request):
+    return render(request, 'hunarbaaz/employer_register.html')
+
+# Login page view
+def login_view(request):
+    return render(request, 'hunarbaaz/login.html')
 # Create your views here.
