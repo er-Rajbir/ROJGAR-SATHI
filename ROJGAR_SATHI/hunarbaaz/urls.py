893b6b40a7596bbe8b38bd442479ee8bea220916
er-Rajbir <anandrajbir13@gmail.com>
2025-06-30 22:32:03 +0530

    logout feature added and on pic text updated

--- ROJGAR_SATHI/hunarbaaz/urls.py
+++ ROJGAR_SATHI/hunarbaaz/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path 
 from . import views
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LogoutView
+
 
 
 urlpatterns = [
@@ -14,6 +16,6 @@ urlpatterns = [
     path('privacy-terms/', views.privacy_terms_view, name='privacy_terms'),
     
     path('login/', auth_views.LoginView.as_view(template_name='appname/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
+    path('logout/',LogoutView.as_view(next_page='home'), name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
 ]
