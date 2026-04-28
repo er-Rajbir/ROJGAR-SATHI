4abbb8f1df1b0b1a24527a81916c271a29d80c7a
er-Rajbir <anandrajbir13@gmail.com>
2026-04-28 20:36:00 +0530

    fixed deploy python version

--- ROJGAR_SATHI/settings.py
+++ ROJGAR_SATHI/settings.py
@@ -48,10 +48,11 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
+    
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
